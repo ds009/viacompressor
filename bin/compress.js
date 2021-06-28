55bcf8c3a0258566eb52e9182e5bbb0d9d786e7b
dengshen <dengshendengshen@gmail.com>
2021-06-28 14:17:40 +0800

    feat: compare compressed size

--- bin/compress.js
+++ bin/compress.js
@@ -1,65 +1,69 @@
 const fs = require('fs')
-const squoosh  = require('@squoosh/lib');
+const squoosh = require('@squoosh/lib');
 const {ImagePool} = squoosh;
 const extMap = {
-  jpg:'mozjpeg',
-  jpeg:'mozjpeg',
-  png:'oxipng',
+  jpg: 'mozjpeg',
+  jpeg: 'mozjpeg',
+  png: 'oxipng',
 }
 const imagePool = new ImagePool();
 
 function handlePath(resourcePath) {
   const index = resourcePath.lastIndexOf('/')
-  return {folder:resourcePath.slice(0, index),name:resourcePath.slice(index+1)}
+  return {folder: resourcePath.slice(0, index), name: resourcePath.slice(index + 1)}
 }
-const compress =  async function (file, options) {
 
-  const {quality,minSize,limitSize}= options;
+const compress = async function (file, options) {
+
+  const {quality, minSize, limitSize} = options;
   const currentSize = fs.statSync(file).size;
 
   const mega = 1024 * 1024
-  if(currentSize < (minSize) * mega){
+  if (currentSize < (minSize) * mega) {
     // 小于minSize不处理
     return;
-  }else if (currentSize > (limitSize) * mega){
-    console.log('!!!Image size too large: '+file);
+  } else if (currentSize > (limitSize) * mega) {
+    console.log('!!!Image size too large: ' + file);
     // show alert and go on
   }
-  const {folder,name} = handlePath(file);
-  const infoPath = folder+'/min.json';
+  const {folder, name} = handlePath(file);
+  const infoPath = folder + '/min.json';
   let oldInfo = {};
   try {
     const infoFile = fs.readFileSync(infoPath);
-    if(infoFile){
+    if (infoFile) {
       oldInfo = JSON.parse(infoFile);
-      if(oldInfo[name]===currentSize){
+      if (oldInfo[name] === currentSize) {
         // compressed before
         return;
       }
     }
-  }catch (e) {
+  } catch (e) {
     // ignore when no info file is found
   }
-
+  console.log('compressing image : '+ file);
   const image = imagePool.ingestImage(file);
   const encodeType = extMap[name.split('.').pop()];
   await image.decoded;
-  await image.encode({[encodeType]:{quality}});
+  await image.encode({[encodeType]: {quality}});
   const encoded = await image.encodedWith[encodeType]
   const rawEncodedImage = encoded.binary;
-  fs.writeFileSync(file, rawEncodedImage);
+  const compressedSize = encoded.size;
+  if (compressedSize < currentSize) {
+    fs.writeFileSync(file, rawEncodedImage);
+  }
   // json might be changed by other process
   let currentInfo = {}
   try {
     const currentInfoFile = fs.readFileSync(infoPath);
     currentInfo = JSON.parse(currentInfoFile);
-  }catch (e) {
+  } catch (e) {
     // ignore when no info file is found
   }
-  currentInfo[name] = encoded.size;
+  currentInfo[name] = Math.min(currentSize, compressedSize);
   fs.writeFileSync(infoPath, JSON.stringify(currentInfo, null, 2), {flag: 'w+'});
 }
-const close = ()=>imagePool.close();
+const close = () => imagePool.close();
 module.exports = {
   compress,
   close,
